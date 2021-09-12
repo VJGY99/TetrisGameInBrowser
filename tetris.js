ab774d0b4df9a0bb4739ca4c5060faa3ccaeb667
VJGY99 <80971475+VJGY99@users.noreply.github.com>
2021-09-12 21:22:19 +0200

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
-
+//
 context.scale(20, 20);
-
+//
 function arenaSweep() {
     let rowCount = 1;
     outer: for (let y = arena.length -1; y > 0; --y) {
